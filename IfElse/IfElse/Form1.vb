461d9c06f570b6199f94911c7fcae19d8a7c2e61
idsc3101 <idsc3101@gmail.com>
2014-09-17 15:26:26 -0500

    Larger number feature

--- IfElse/IfElse/Form1.vb
+++ IfElse/IfElse/Form1.vb
@@ -11,9 +11,16 @@ Public Class Form1
         num1 = CDbl(st1)
         num2 = CDbl(st2)
 
-        ' Your code here
+        If num1 < num2 Then
+            largerNum = num2
+        Else
+            largerNum = num1
+        End If
 
-        txtResult.Text = "Not implemented yet"
+        txtResult.Text = "The larger number is " & largerNum
     End Sub
 
+    Private Sub txtFirstNum_TextChanged(sender As Object, e As EventArgs) Handles txtFirstNum.TextChanged
+
+    End Sub
 End Class
